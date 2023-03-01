9885c26306c355d94f4d4f8cdbb02cb9a8c9e387
Denis Wolter <wolter.important@gmail.com>
2023-03-01 16:12:53 +0300

    add googleEvents (static)

--- src/components/Content/Content.tsx
+++ src/components/Content/Content.tsx
@@ -2,12 +2,14 @@ import React from "react";
 import styles from "./content.module.css"
 import InputCity from "../InputCity/InputCity";
 import TimeAndDate from "../TimeAndDate/TimeAndDate";
+import GoogleEvents from "../GoogleEvents/GoogleEvents";
 
 const Content = () => {
     return (
         <div className={styles.content}>
             <InputCity />
             <TimeAndDate />
+            <GoogleEvents />
         </div>
     )
 }
