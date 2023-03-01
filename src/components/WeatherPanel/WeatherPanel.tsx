a089730e4691e4f2354fe151d98d05c7e8567cd5
Denis Wolter <wolter.important@gmail.com>
2023-03-01 22:30:02 +0300

    add date/time buttons

--- src/components/WeatherPanel/WeatherPanel.tsx
+++ src/components/WeatherPanel/WeatherPanel.tsx
@@ -2,67 +2,73 @@ import styles from "./WeatherPanel.module.css"
 
 const WeatherPanel = () => {
     return (
-        <div className={styles.panel}>
-            <div className={styles.leftContainer}>
-                <img className={styles.imgMain} src="/img/sun.png" alt="Sun"/>
-                <div className={styles.todayContainer}>
-                    <p className={styles.textToday}>Today</p>
-                    <p className={styles.textDegree}>12°</p>
+        <>
+            <div className={styles.topButtons}>
+                <p className={styles.textTopDate}>date</p>
+                <p className={styles.textTopTime}>time</p>
+            </div>
+            <div className={styles.panel}>
+                <div className={styles.leftContainer}>
+                    <img className={styles.imgMain} src="/img/sun.png" alt="Sun"/>
+                    <div className={styles.todayContainer}>
+                        <p className={styles.textToday}>Today</p>
+                        <p className={styles.textDegree}>12°</p>
+                    </div>
+                </div>
+                <div className={styles.rightContainer}>
+                    <div className={styles.containerDays}>
+                        <p className={styles.textDays}>Thu</p>
+                        <img className={styles.imgSmallDays} src="/img/partly-cloudy.png" alt="partly-cloudy"/>
+                        <p className={styles.textDaysDegree}>11°</p>
+                    </div>
+                    <div className={styles.containerDays}>
+                        <p className={styles.textDays}>Thu</p>
+                        <img className={styles.imgSmallDays} src="/img/partly-cloudy.png" alt="partly-cloudy"/>
+                        <p className={styles.textDaysDegree}>11°</p>
+                    </div>
+                    <div className={styles.containerDays}>
+                        <p className={styles.textDays}>Thu</p>
+                        <img className={styles.imgSmallDays} src="/img/partly-cloudy.png" alt="partly-cloudy"/>
+                        <p className={styles.textDaysDegree}>11°</p>
+                    </div>
+                    <div className={styles.containerDays}>
+                        <p className={styles.textDays}>Thu</p>
+                        <img className={styles.imgSmallDays} src="/img/partly-cloudy.png" alt="partly-cloudy"/>
+                        <p className={styles.textDaysDegree}>11°</p>
+                    </div>
+                    <div className={styles.containerDays}>
+                        <p className={styles.textDays}>Thu</p>
+                        <img className={styles.imgSmallDays} src="/img/partly-cloudy.png" alt="partly-cloudy"/>
+                        <p className={styles.textDaysDegree}>11°</p>
+                    </div>
+                    <div className={styles.containerDays}>
+                        <p className={styles.textDays}>Thu</p>
+                        <img className={styles.imgSmallDays} src="/img/partly-cloudy.png" alt="partly-cloudy"/>
+                        <p className={styles.textDaysDegree}>11°</p>
+                    </div>
+                    <div className={styles.containerDays}>
+                        <p className={styles.textDays}>Thu</p>
+                        <img className={styles.imgSmallDays} src="/img/partly-cloudy.png" alt="partly-cloudy"/>
+                        <p className={styles.textDaysDegree}>11°</p>
+                    </div>
+                    <div className={styles.containerDays}>
+                        <p className={styles.textDays}>Thu</p>
+                        <img className={styles.imgSmallDays} src="/img/partly-cloudy.png" alt="partly-cloudy"/>
+                        <p className={styles.textDaysDegree}>11°</p>
+                    </div>
+                    <div className={styles.containerDays}>
+                        <p className={styles.textDays}>Thu</p>
+                        <img className={styles.imgSmallDays} src="/img/partly-cloudy.png" alt="partly-cloudy"/>
+                        <p className={styles.textDaysDegree}>11°</p>
+                    </div>
+                    <div className={styles.containerDays}>
+                        <p className={styles.textDays}>Thu</p>
+                        <img className={styles.imgSmallDays} src="/img/partly-cloudy.png" alt="partly-cloudy"/>
+                        <p className={styles.textDaysDegree}>11°</p>
+                    </div>
                 </div>
             </div>
-            <div className={styles.rightContainer}>
-                <div className={styles.containerDays}>
-                    <p className={styles.textDays}>Thu</p>
-                    <img className={styles.imgSmallDays} src="/img/partly-cloudy.png" alt="partly-cloudy"/>
-                    <p className={styles.textDaysDegree}>11°</p>
-                </div>
-                <div className={styles.containerDays}>
-                    <p className={styles.textDays}>Thu</p>
-                    <img className={styles.imgSmallDays} src="/img/partly-cloudy.png" alt="partly-cloudy"/>
-                    <p className={styles.textDaysDegree}>11°</p>
-                </div>
-                <div className={styles.containerDays}>
-                    <p className={styles.textDays}>Thu</p>
-                    <img className={styles.imgSmallDays} src="/img/partly-cloudy.png" alt="partly-cloudy"/>
-                    <p className={styles.textDaysDegree}>11°</p>
-                </div>
-                <div className={styles.containerDays}>
-                    <p className={styles.textDays}>Thu</p>
-                    <img className={styles.imgSmallDays} src="/img/partly-cloudy.png" alt="partly-cloudy"/>
-                    <p className={styles.textDaysDegree}>11°</p>
-                </div>
-                <div className={styles.containerDays}>
-                    <p className={styles.textDays}>Thu</p>
-                    <img className={styles.imgSmallDays} src="/img/partly-cloudy.png" alt="partly-cloudy"/>
-                    <p className={styles.textDaysDegree}>11°</p>
-                </div>
-                <div className={styles.containerDays}>
-                    <p className={styles.textDays}>Thu</p>
-                    <img className={styles.imgSmallDays} src="/img/partly-cloudy.png" alt="partly-cloudy"/>
-                    <p className={styles.textDaysDegree}>11°</p>
-                </div>
-                <div className={styles.containerDays}>
-                    <p className={styles.textDays}>Thu</p>
-                    <img className={styles.imgSmallDays} src="/img/partly-cloudy.png" alt="partly-cloudy"/>
-                    <p className={styles.textDaysDegree}>11°</p>
-                </div>
-                <div className={styles.containerDays}>
-                    <p className={styles.textDays}>Thu</p>
-                    <img className={styles.imgSmallDays} src="/img/partly-cloudy.png" alt="partly-cloudy"/>
-                    <p className={styles.textDaysDegree}>11°</p>
-                </div>
-                <div className={styles.containerDays}>
-                    <p className={styles.textDays}>Thu</p>
-                    <img className={styles.imgSmallDays} src="/img/partly-cloudy.png" alt="partly-cloudy"/>
-                    <p className={styles.textDaysDegree}>11°</p>
-                </div>
-                <div className={styles.containerDays}>
-                    <p className={styles.textDays}>Thu</p>
-                    <img className={styles.imgSmallDays} src="/img/partly-cloudy.png" alt="partly-cloudy"/>
-                    <p className={styles.textDaysDegree}>11°</p>
-                </div>
-            </div>
-        </div>
+        </>
     )
 }
 
