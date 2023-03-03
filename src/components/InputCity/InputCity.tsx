37408959608b4f8c52a3792d486af4aeba3fc1d6
Denis Wolter <wolter.important@gmail.com>
2023-03-03 08:59:11 +0300

    add searchButton in inputCity

--- src/components/InputCity/InputCity.tsx
+++ src/components/InputCity/InputCity.tsx
@@ -3,7 +3,10 @@ import styles from "./InputCity.module.css"
 const InputCity = () => {
     return (
         <div className={styles.inputBackground}>
-            <input placeholder="someCity" className={styles.input} />
+            <div className={styles.inputContent}>
+                <input placeholder="Write city" className={styles.input} />
+                <input type="image" src="/img/search.png" className={styles.imgSearch} alt="search"/>
+            </div>
             <hr className={styles.line}/>
         </div>
     )
